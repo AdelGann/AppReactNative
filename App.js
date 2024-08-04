5df0b8097f5b41d677b89678ceeeb848ad543640
Adel Chafic Gannem <adelchaficgc@gmail.com>
2024-08-04 17:40:53 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image , Button} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button 
+       color={"black"}
+       title="Button"
+       onPress={() => {alert("hola")}}/>
     </View>
   );
 }
@@ -13,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
